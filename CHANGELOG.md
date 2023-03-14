8ed473afd296079f660013b297235338efe011f8
ali.aghapour <alagha911@gmail.com>
2023-03-14 23:32:33 +0330

    first implementation of the hashing and comparison functions with typescript

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## 0.1.0
+
+* first implementation of the hashing and comparison functions with typescript
---
+++ index.ts
@@ -0,0 +1,36 @@
+import md5 from "md5";
+
+interface HashPassOptions {
+  salt: number;
+  rounds: number;
+}
+
+export const hashPass = (
+  rawPassword: string,
+  { salt = 20, rounds = 15 }: HashPassOptions = { salt: 20, rounds: 15 }
+): string => {
+  let hashed = md5(rawPassword + salt);
+  for (let i = 0; i <= rounds; i++) {
+    hashed = md5(hashed);
+  }
+
+  return `${salt}$${rounds}$${hashed}`;
+};
+
+export const compare = (
+  rawPassword: string,
+  hashedPassword: string
+): boolean => {
+  try {
+    const [salt, rounds] = hashedPassword.split("$");
+
+    const hashedRawPassword = hashPass(rawPassword, {
+      salt: Number(salt),
+      rounds: Number(rounds),
+    });
+
+    return hashedPassword === hashedRawPassword;
+  } catch (error) {
+    throw Error(error.message);
+  }
+};
